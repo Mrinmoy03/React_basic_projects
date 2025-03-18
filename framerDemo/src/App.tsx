7e36ee1f6c9051402c23216e1aa6b0648803766b
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-03-18 12:30:41 +0530

    Add files via upload

--- framerDemo/src/App.tsx
+++ framerDemo/src/App.tsx
@@ -1,10 +1,32 @@
 
-import { BouincingBorder } from "./Components/BouincingBorder"
-import { Gesture } from "./Components/Gesture"
-import { PulsingButton } from "./Components/PulsingButton"
-import VisibleCircle from "./Components/VisibleCircle"
-import { AnimatedCard } from "./examples/AnimatedCard"
-import FlippingCard from "./examples/FlippingCard"
+// import { BouincingBorder } from "./Components/BouincingBorder"
+// import { Gesture } from "./Components/Gesture"
+// import { PulsingButton } from "./Components/PulsingButton"
+// import { StaggerAnimation } from "./Components/StaggerAnimation"
+// import VisibleCircle from "./Components/VisibleCircle"
+// import { AnimatedCard } from "./examples/AnimatedCard"
+// import { AnimatedGallery } from "./examples/AnimatedGallery"
+// import { AnimatedShape } from "./examples/AnimatedShape"
+// import FlippingCard from "./examples/FlippingCard"
+// import { ImageGallery } from "./examples/ImageGallery"
+
+// import { BouncingCircle } from "./Practice/TRansformations/BouncingCircle"
+// import { Box } from "./Practice/TRansformations/Box"
+// import { ComplexAnimation } from "./Practice/TRansformations/ComplexAnimation"
+// import { SequentialBoxes } from "./Practice/TRansformations/SequentialBoxes"
+// import { SkewRectangle } from "./Practice/TRansformations/SkewRectangle"
+// import { SpinningIcon } from "./Practice/TRansformations/SpinningIcon"
+
+// import { AnimatedCard } from "./Components/ScrollTriggerAnimation/AnimatedCard"
+// import { AnimationScroll } from "./Components/ScrollTriggerAnimation/AnimationScroll"
+// import { Scroll } from "./Components/ScrollTriggerAnimation/Scroll"
+// import { ScrollAnimation } from "./Components/ScrollTriggerAnimation/ScrollAnimation"
+// import { UseTransform } from "./Components/UseTransform"
+// import { UseSpringHook1 } from "./examples/UsePringHook1"
+
+// import { RangeSlider, RangeSlider2 } from "./examples/RangeSlider"
+
+// import { UseMotionValue } from "./Components/UseMotionValue"
 
 function App() {
 
@@ -17,9 +39,57 @@ function App() {
     {/* <FlippingCard/> */}
     {/* <Gesture/> */}
     {/* <AnimatedCard/> */}
+    {/* <div className="min-h-screen flex items-center justify-center">
+    <ImageGallery/>
+    </div> */}
+    {/* <AnimatedShape/> */}
+    {/* <StaggerAnimation/> */}
+    {/* <AnimatedGallery/> */}
+    {/* <UseMotionValue/> */}
+    {/* <RangeSlider/> */}
+    
+    {/* <RangeSlider2/> */}
+   {/* <UseSpringHook1/> */}
+   {/* <UseTransform/> */}
 
+   {/* ----scrooll trigger Animation -----*/}
+   {/* <div>
+       <h1 className="text-center text-3xl mb-4">
+        Scroll Down to see Animation</h1> 
+        <div className="h-screen"></div>
+        
+    </div>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+   <AnimatedCard/> */}
+
+   {/* -----use scroll Hook ----- */}
+   {/* <ScrollAnimation/> */}
+   {/* <Scroll/> */}
+   {/* <AnimationScroll/> */}
+     {/* make sure to unplug the app.css */}
+
+
+
+
+
+
+
+
+     {/* -------practice-------  */}
+
+     {/* <Box/> */}
+     {/* <BouncingCircle/> */}
+     {/* <SpinningIcon/> */}
+     {/* <SkewRectangle/> */}
+     {/* <ComplexAnimation/> */}
+     {/* <SequentialBoxes/> */}
     </>
   )
 }
 
+
+
 export default App
+
+
+
