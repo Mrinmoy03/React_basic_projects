da212c7e3d11e0416361a086ce9d614bc4f26f12
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-03-18 12:04:02 +0530

    Add files via upload

---
+++ framerDemo/src/Components/ScrollTriggerAnimation/ScrollAnimation.tsx
@@ -0,0 +1,66 @@
+import { useMotionValueEvent, useScroll } from 'framer-motion'
+import React from 'react'
+
+export const ScrollAnimation = () => {
+ const {scrollY} =  useScroll()
+ useMotionValueEvent(scrollY, 'change',(latest)=>{
+    console.log("page scroll: ", latest)
+ })
+  return (
+    <div>
+        <h1>Scroll Animation</h1>
+        <ul className='h-screen'>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
